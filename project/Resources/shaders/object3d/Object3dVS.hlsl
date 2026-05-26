df57374096cb49485659432faceec0337ff559a3
Aoyagi Gakuto <k024g0082@g.neec.ac.jp>
2026-05-26 09:47:27 +0900

    hlslが入っていなかった

--- project/Resources/shaders/object3d/Object3dVS.hlsl
+++ project/Resources/shaders/object3d/Object3dVS.hlsl
@@ -4,10 +4,8 @@ struct TransformationMatrix
 {
     float4x4 WVP;
     float4x4 World;
-    float4x4 WorldInverseTranspose; // 非均一スケール対応（法線変換用）
-    float4x4 LightVP;
+    float4x4 LightVP; // ライト空間のビュープロジェクション行列
 };
-
 ConstantBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
 
 struct VertexShaderInput
@@ -22,7 +20,7 @@ VertexShaderOutput main(VertexShaderInput input)
     VertexShaderOutput output;
     output.position      = mul(input.position, gTransformationMatrix.WVP);
     output.texcoord      = input.texcoord;
-    output.normal        = normalize(mul(input.normal, (float3x3)gTransformationMatrix.WorldInverseTranspose));
+    output.normal        = normalize(mul(input.normal, (float3x3)gTransformationMatrix.World));
     output.worldPos      = mul(input.position, gTransformationMatrix.World).xyz;
     output.lightSpacePos = mul(input.position, gTransformationMatrix.LightVP);
     return output;
