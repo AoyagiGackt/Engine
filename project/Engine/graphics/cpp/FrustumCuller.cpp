96edefd8bbc7be173bd13a04d3afbfcae31cd4a2
Aoyagi Gakuto <k024g0082@g.neec.ac.jp>
2026-06-25 13:02:48 +0900

    がったいさせたときのエラー修正

--- project/Engine/graphics/cpp/FrustumCuller.cpp
+++ project/Engine/graphics/cpp/FrustumCuller.cpp
@@ -3,8 +3,7 @@
 
 void FrustumCuller::Update(const Matrix4x4& m)
 {
-    // Gribb-Hartmann method (row-vector convention: v * M)
-    // row i of m is m.m[i][*]
+
     // left:   row3 + row0
     planes_[0] = { m.m[0][3] + m.m[0][0], m.m[1][3] + m.m[1][0], m.m[2][3] + m.m[2][0], m.m[3][3] + m.m[3][0] };
     // right:  row3 - row0
@@ -13,7 +12,7 @@ void FrustumCuller::Update(const Matrix4x4& m)
     planes_[2] = { m.m[0][3] + m.m[0][1], m.m[1][3] + m.m[1][1], m.m[2][3] + m.m[2][1], m.m[3][3] + m.m[3][1] };
     // top:    row3 - row1
     planes_[3] = { m.m[0][3] - m.m[0][1], m.m[1][3] - m.m[1][1], m.m[2][3] - m.m[2][1], m.m[3][3] - m.m[3][1] };
-    // near:   row2 only (DirectX NDC: near plane at z=0, not z=-1 as in OpenGL)
+    // near:   row2
     planes_[4] = { m.m[0][2], m.m[1][2], m.m[2][2], m.m[3][2] };
     // far:    row3 - row2
     planes_[5] = { m.m[0][3] - m.m[0][2], m.m[1][3] - m.m[1][2], m.m[2][3] - m.m[2][2], m.m[3][3] - m.m[3][2] };
@@ -30,7 +29,6 @@ void FrustumCuller::Update(const Matrix4x4& m)
 bool FrustumCuller::IsVisible(const Vector3& center, const Vector3& half) const
 {
     for (const auto& p : planes_) {
-        // positive vertex: corner furthest in plane normal direction
         float px = (p.a >= 0.0f) ? (center.x + half.x) : (center.x - half.x);
         float py = (p.b >= 0.0f) ? (center.y + half.y) : (center.y - half.y);
         float pz = (p.c >= 0.0f) ? (center.z + half.z) : (center.z - half.z);
